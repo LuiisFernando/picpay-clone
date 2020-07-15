32f28a67fd5afaa79c7ecdcad64c86a83e5b33a7
Luis Fernando <lsh.fernandors@gmail.com>
2020-07-15 12:51:05 -0300

    implementing bottom tab and payButton

---
+++ src/components/PayButton/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { TouchableWithoutFeedback } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Button, Label } from './styles';
+
+export default function PayButton({ onPress, focused }) {
+    return (
+        <TouchableWithoutFeedback onPress={onPress}>
+            <Button
+                colors={
+                    focused
+                     ? ['#FFF', '#ccc']
+                     : ['#00fc6c', '#00ac4a']
+                }
+                start={{x: 1, y: 1}} end={{x: 0.2, y: 0.2}}
+            >
+                <Icon name="attach-money" size={30} color={focused ? '#000' : '#FFF'} />
+                <Label focused={focused}>Pagar</Label>
+            </Button>
+        </TouchableWithoutFeedback>
+    );
+}
---
+++ src/screens/Pay/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+export default function Pay() {
+    return (
+        <View>
+            <Text>Pay</Text>
+        </View>
+    );
+}
